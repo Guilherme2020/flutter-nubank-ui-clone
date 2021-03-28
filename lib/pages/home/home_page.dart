5b35e24822ead631d3b3a67d9258a03d1b2fa794
Guilherme Rodrigues Simeão <guilhermerodrigues123477@gmail.com>
2021-03-28 15:32:00 -0300

    feat - create appbar component

---
+++ lib/pages/home/home_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:nubank_ui/pages/home/widgets/my_app_bar.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool _showMenu;
+
+  @override
+  void initState() {
+    super.initState();
+    _showMenu = false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.purple[800],
+      body: Stack(alignment: Alignment.topCenter, children: <Widget>[
+        MyAppBar(
+          showMenu: _showMenu,
+          onTap: () {
+            setState(() {
+              _showMenu = !_showMenu;
+            });
+          },
+        ),
+      ]),
+    );
+  }
+}
---
+++ lib/pages/home/widgets/my_app_bar.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class MyAppBar extends StatelessWidget {
+  final bool showMenu;
+  final VoidCallback onTap;
+  const MyAppBar({Key key, this.showMenu, this.onTap}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      SizedBox(
+        height: MediaQuery.of(context).padding.top,
+      ),
+      GestureDetector(
+        onTap: onTap,
+        child: Container(
+            color: Colors.purple[800],
+            height: 120,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image.network(
+                        'https://logodownload.org/wp-content/uploads/2019/08/nubank-logo-3.png',
+                        height: 30,
+                        color: Colors.white),
+                    SizedBox(width: 10),
+                    Text('',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 18))
+                  ],
+                ),
+                Icon(!showMenu ? Icons.expand_more : Icons.expand_less)
+              ],
+            )),
+      )
+    ]);
+  }
+}
